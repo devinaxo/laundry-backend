c161ed21bac4cce326c8c8d6aba47f6278c5560a
devinaxo <brian456251@gmail.com>
2025-09-12 13:58:28 -0300

    what

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,7 +10,7 @@ use Laravel\Sanctum\PersonalAccessToken;
 class AuthController extends Controller {
     public function login(LoginRequest $request) {
         PersonalAccessToken::where('expires_at', '<', now())->delete();
-        
+
         $validated = $request->validated();
 
         $credentials = array_merge($validated, ['active' => true]);
@@ -39,5 +39,11 @@ class AuthController extends Controller {
         }
 
         return response()->json(['error' => 'User not authenticated'], 401);
+    }
+
+    public function user(Request $request) {
+        $user = $request->user();
+
+        return response()->json($user->load('role.permissions'));
     }
 }
