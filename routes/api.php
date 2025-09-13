417362011d7363233ee074e58e0a34bcce2081f2
devinaxo <brian456251@gmail.com>
2025-09-13 16:59:48 -0300

    changed routes to use apiResource

--- routes/api.php
+++ routes/api.php
@@ -20,42 +20,56 @@ Route::middleware('auth:sanctum')->group(function () {
     /**
      * User Endpoints
      */
-    Route::get('/users', [UserController::class, 'index'])->middleware('permission:viewUser');
+    // Custom routes must be defined before resource routes to avoid conflicts
     Route::get('/users/all', [UserController::class, 'all'])->middleware('permission:viewUser');
-    Route::get('/users/{user}', [UserController::class, 'show'])->middleware('permission:viewUser');
-    Route::post('/users', [UserController::class, 'store'])->middleware('permission:createUser');
-    Route::patch('/users/{user}', [UserController::class, 'update'])->middleware('permission:editUser');
-    Route::delete('/users/{user}', [UserController::class, 'destroy'])->middleware('permission:deleteUser');
     Route::patch('/users/{user}/restore', [UserController::class, 'restore'])->middleware('permission:editUser');
+    
+    Route::apiResource('users', UserController::class)->middleware([
+        'index' => 'permission:viewUser',
+        'show' => 'permission:viewUser',
+        'store' => 'permission:createUser',
+        'update' => 'permission:editUser',
+        'destroy' => 'permission:deleteUser',
+    ]);
 
     /**
      * Role Endpoints
      */
-    Route::get('/roles', [RoleController::class, 'index'])->middleware('permission:viewRole');
-    Route::get('/roles/{role}', [RoleController::class, 'show'])->middleware('permission:viewRole');
-    Route::post('/roles', [RoleController::class, 'store'])->middleware('permission:createRole');
-    Route::patch('/roles/{role}', [RoleController::class, 'update'])->middleware('permission:editRole');
+    // Custom routes must be defined before resource routes to avoid conflicts
     Route::patch('/roles/{role}/permissions', [RoleController::class, 'assignPermissions'])->middleware('permission:editRole');
+    
+    Route::apiResource('roles', RoleController::class)->middleware([
+        'index' => 'permission:viewRole',
+        'show' => 'permission:viewRole',
+        'store' => 'permission:createRole',
+        'update' => 'permission:editRole',
+    ]);
 
     /**
      * Permission Endpoints
      */
-    Route::get('/permissions', [PermissionController::class, 'index'])->middleware('permission:viewPermission');
-    Route::get('/permissions/{permission}', [PermissionController::class, 'show'])->middleware('permission:viewPermission');
-    Route::post('/permissions', [PermissionController::class, 'store'])->middleware('permission:createPermission');
-    Route::patch('/permissions/{permission}', [PermissionController::class, 'update'])->middleware('permission:editPermission');
+    Route::apiResource('permissions', PermissionController::class)->middleware([
+        'index' => 'permission:viewPermission',
+        'show' => 'permission:viewPermission',
+        'store' => 'permission:createPermission',
+        'update' => 'permission:editPermission',
+    ]);
 
     /**
      * Client Endpoints
      */
-    Route::get('/clients', [ClientController::class, 'index'])->middleware('permission:viewClient');
+    // Custom routes must be defined before resource routes to avoid conflicts
     Route::get('/clients/all', [ClientController::class, 'all'])->middleware('permission:viewClient');
     Route::get('/clients/paginated', [ClientController::class, 'paginated'])->middleware('permission:viewClient');
-    Route::get('/clients/{client}', [ClientController::class, 'show'])->middleware('permission:viewClient');
-    Route::post('/clients', [ClientController::class, 'store'])->middleware('permission:createClient');
-    Route::patch('/clients/{client}', [ClientController::class, 'update'])->middleware('permission:editClient');
-    Route::delete('/clients/{client}', [ClientController::class, 'destroy'])->middleware('permission:deleteClient');
     Route::patch('/clients/{client}/restore', [ClientController::class, 'restore'])->middleware('permission:editClient');
+    
+    Route::apiResource('clients', ClientController::class)->middleware([
+        'index' => 'permission:viewClient',
+        'show' => 'permission:viewClient',
+        'store' => 'permission:createClient',
+        'update' => 'permission:editClient',
+        'destroy' => 'permission:deleteClient',
+    ]);
 });
 
 Route::get('/test-user', function () {
