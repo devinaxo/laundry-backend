1ebd335b2f67365c671496c1c71ddd3e23a323e6
devinaxo <brian456251@gmail.com>
2025-09-14 19:28:42 -0300

    changed current user route

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', [AuthController::class, 'user']);
+    Route::get('/currentUser', [AuthController::class, 'user']);
 });
 
 Route::middleware('auth:sanctum')->group(function () {
